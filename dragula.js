e23dea9e630bbb28495780fdc46b892ca709e98b
Efimija Laktash <efi.laktas531@gmail.com>
2018-09-14 15:38:33 +0200

    Updating copy-paste func.

--- dragula.js
+++ dragula.js
@@ -377,6 +377,14 @@ function dragula(initialContainers, options) {
       _lastDropTarget = dropTarget;
       over();
     }
+
+    if (dropTarget && dropTarget.hasAttribute('can-copy')) {
+      var canCopy = dropTarget.getAttribute('can-copy');
+      if (canCopy === 'false') {
+        return;
+      }
+    }
+
     var parent = getParent(item);
     var reference;
     var immediate = getImmediateChild(dropTarget, elementBehindCursor);
